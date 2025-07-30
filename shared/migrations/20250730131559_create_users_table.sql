118329b60b19932530cee5cbf212854658c9d6c4
Ugo Tiberto <ugo.tiberto@gmail.com>
2025-07-30 21:31:23 +0200

    fix: added if not exist to create table users

--- shared/migrations/20250730131559_create_users_table.sql
+++ shared/migrations/20250730131559_create_users_table.sql
@@ -1,7 +1,7 @@
 -- migrations/20250730131559_create_users_table.sql
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     email TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL
